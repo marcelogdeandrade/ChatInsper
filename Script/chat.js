12fc4f7749246d7b3169f55a03450355a1d2c522
marcelogdeandrade <marcelogdeandrade@github.com>
2016-11-21 16:19:32 -0200

    first commit

---
+++ Script/chat.js
@@ -0,0 +1,51 @@
+var Message;
+Message = function (arg) {
+    this.text = arg.text, this.message_side = arg.message_side;
+    this.draw = function (_this) {
+        return function () {
+            var $message;
+            $message = $($('.message_template').clone().html());
+            $message.addClass(_this.message_side).find('.text').html(_this.text);
+            $('.messages').append($message);
+            return setTimeout(function () {
+                return $message.addClass('appeared');
+            }, 0);
+        };
+    }(this);
+    return this;
+};
+var getMessageText, message_side, sendMessage;
+message_side = 'right';
+getMessageText = function () {
+    var $message_input;
+    $message_input = $('.message_input');
+    return $message_input.val();
+};
+sendMessage = function (text) {
+    var $messages, message;
+    if (text.trim() === '') {
+        return;
+    }
+    $('.message_input').val('');
+    $messages = $('.messages');
+    message_side = message_side === 'left' ? 'right' : 'left';
+    message = new Message({
+        text: text,
+        message_side: message_side
+    });
+    message.draw();
+    return $messages.animate({ scrollTop: $messages.prop('scrollHeight') }, 300);
+};
+$('.send_message').click(function (e) {
+    socket.emit('chat message', getMessageText());
+    return false;
+});
+var socket = io();
+$('form').submit(function(){
+    socket.emit('chat message', $('#m').val());
+    $('#m').val('');
+    return false;
+});
+socket.on('chat message', function(msg){
+    sendMessage(msg);
+});
---
+++ server.js
@@ -0,0 +1,25 @@
+var express = require('express')
+var app = express()
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.use(express.static(__dirname + '/View'));
+app.use('/style',express.static(__dirname + '/Script'));
+
+app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js')); // redirect bootstrap JS
+app.use('/js', express.static(__dirname + '/node_modules/jquery/dist')); // redirect JS jQuery
+app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css')); // redirect CSS bootstrap
+
+app.get('/', function(req, res){
+  res.sendFile('/index.html');
+});
+
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
+});
+
+http.listen(3000,'0.0.0.0', function(){
+  console.log('listening on *:3000');
+});
