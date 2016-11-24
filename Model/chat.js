03af5920facf411d119d979a8576673794e1199d
marcelogdeandrade <marcelogdeandrade@github.com>
2016-11-24 16:57:46 -0200

    added angular and added name after connecting.

--- Model/chat.js
+++ Model/chat.js
@@ -1,14 +1,61 @@
+var nome;
+
+//Angular
+angular.module('myApp', ['ngMaterial'])
+
+.controller('AppCtrl', function($scope, $mdDialog) {
+  $scope.status = '  ';
+  $scope.customFullscreen = false;
+
+  $scope.showPrompt = function() {
+   // Appending dialog to document.body to cover sidenav in docs app
+   var confirm = $mdDialog.prompt()
+     .title('Qual é o seu nome?')
+    //  .textContent('Bowser is a common name.')
+     .placeholder('Seu nome')
+     .ariaLabel('Seu Nome')
+    // .initialValue('Buddy')
+    //.targetEvent(ev)
+    //  .ok('Okay!')
+    //  .cancel('IOk');
+
+   $mdDialog.show(confirm).then(function(result) {
+     nome = result.slice(0,2).toUpperCase();
+     $scope.status = 'You decided to name your dog ' + result + '.';
+   }, function() {
+     $scope.status = 'You didn\'t name your dog.';
+   });
+ };
+
+  function DialogController($scope, $mdDialog) {
+    $scope.hide = function() {
+      $mdDialog.hide();
+    };
+
+    $scope.cancel = function() {
+      $mdDialog.cancel();
+    };
+
+    $scope.answer = function(answer) {
+      $mdDialog.hide(answer);
+    };
+  }
+});
+
+//SOCKET IO
+
 var Message;
 var socket = io();
 // Message function
 Message = function (arg) {
-    this.text = arg.text, this.message_side = arg.message_side;
+    this.text = arg.text, this.message_side = arg.message_side, this.name = arg.name;
     this.draw = function (_this) {
         return function () {
             var $message;
             $message = $($('.message_template').clone().html());
             $message.addClass(_this.message_side).find('.text').html(_this.text);
             $('.messages').append($message);
+            $message.find('.avatar').html(this.name);
             return setTimeout(function () {
                 return $message.addClass('appeared');
             }, 0);
@@ -27,7 +74,7 @@ getMessageText = function () {
 };
 
 // SendMessage function
-sendMessage = function (text, message_side) {
+sendMessage = function (text, message_side, name) {
     var $messages, message;
     if (text.trim() === '') {
         return;
@@ -35,7 +82,8 @@ sendMessage = function (text, message_side) {
     $messages = $('.messages');
     message = new Message({
         text: text,
-        message_side: message_side
+        message_side: message_side,
+        name : name
     });
     message.draw();
     return $messages.animate({ scrollTop: $messages.prop('scrollHeight') }, 300);
@@ -45,7 +93,8 @@ sendMessage = function (text, message_side) {
 $('.send_message').click(function (e) {
     var data={
         text : getMessageText(),
-        id : socket.io.engine.id.toString()
+        id : socket.io.engine.id.toString(),
+        name : nome
         };
     socket.emit('chat message', data);
     $('.message_input').val(''); //Delete input text
@@ -56,7 +105,8 @@ $('.send_message').click(function (e) {
 $('form').submit(function(){
     var data={
         text : getMessageText(),
-        id : socket.io.engine.id.toString()
+        id : socket.io.engine.id.toString(),
+        name : nome
         };
     socket.emit('chat message', data);
     $('.message_input').val(''); //Delete input text
@@ -68,10 +118,13 @@ socket.on('chat message', function(data){
     var text = data.text
     var id = data.id
     if (id == socket.io.engine.id.toString()){
-        sendMessage(text,'right');
+        sendMessage(text,'right',data.name);
     }
     else {
-        sendMessage(text,'left');
+        sendMessage(text,'left', data.name);
     }
+});
 
+socket.on('connect', function(){
+    angular.element(document.getElementById('body')).scope().showPrompt();
 });
--- server.js
+++ server.js
@@ -10,8 +10,14 @@ app.use('/model',express.static(__dirname + '/Model'));
 
 
 //Static paths to dependencies
+// app.use(express.static(__dirname + '/public'));
+
 app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js')); // redirect bootstrap JS
 app.use('/js', express.static(__dirname + '/node_modules/jquery/dist')); // redirect JS jQuery
+app.use('/angular', express.static(__dirname + '/node_modules/angular/')); // redirect to JS Angular
+app.use('/angular', express.static(__dirname + '/node_modules/angular-animate/')); // redirect to JS Angular
+app.use('/angular', express.static(__dirname + '/node_modules/angular-aria/')); // redirect to JS Angular
+app.use('/angular', express.static(__dirname + '/node_modules/angular-material/')); // redirect to JS Angular
 app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css')); // redirect CSS bootstrap
 
 app.get('/', function(req, res){
